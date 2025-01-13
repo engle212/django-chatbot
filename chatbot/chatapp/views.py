aafe17f6286360032b4b81f03490d28c89d6fe6b
engle212 <engle.212@buckeyemail.osu.edu>
2025-01-13 10:55:12 -0500

    Implemented enter button

--- chatbot/chatapp/views.py
+++ chatbot/chatapp/views.py
@@ -6,6 +6,15 @@ def index(request):
   template = loader.get_template("chatapp/index.html")
   context = {
     "title": "django-chatbot",
-    "message": "Hello, world! from views.index()"
+    "messages": [
+      [0, "Hello, world! I am the user"],
+      [1, "Hello, I am a robot"]
+    ],
   }
+  if request.POST:
+    # Reflect new message in view
+    context["messages"].append([0, request.POST["text"]])
+    # Comm with AWS
+    # Get LLM's reply
+  print(context)
   return HttpResponse(template.render(context, request))
