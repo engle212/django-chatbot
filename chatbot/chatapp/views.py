cd61594bc61cafed0e16db36a3c6381050b13ab9
engle212 <engle.212@buckeyemail.osu.edu>
2025-01-15 12:32:21 -0500

    Began json storage

--- chatbot/chatapp/views.py
+++ chatbot/chatapp/views.py
@@ -2,6 +2,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.conf import settings
+import json
+import time
+import os
 
 def index(request):
   """
@@ -13,27 +17,42 @@ def index(request):
     The content to be displayed to the user. Passes in a context and request object to a template.
   """
   template = loader.get_template("chatapp/index.html")
+  session_key = request.session.session_key
+
+  print(get_all_conversations(session_key))
+
+  context = {}
+  #with open("data.json", "r") as infile:
+  #  context = json.load(infile)
   context = {
     "title": "django-chatbot",
     "messages": [
       [0, "Hello, world! I am the user"],
       [1, "Hello, I am a robot"]
-    ],
+    ]
   }
+
   if request.POST:
     user_message = request.POST["text"]
     # Reflect new message in view
     context["messages"].append([0, user_message])
     # Store user message in S3 bucket
-    store_message(request.session.session_key, 3, True, user_message)
+    store_message(session_key, 3, True, user_message)
     # Get LLM's reply
     ai_message = get_reply()
     context["messages"].append([1, ai_message])
     # Store LLM message in S3 bucket
-    store_message(request.session.session_key, 3, False, ai_message)
+    store_message(session_key, 3, False, ai_message)
 
   print(context)
   return HttpResponse(template.render(context, request))
+
+def get_all_conversations(user_id):
+  data_files = os.listdir(os.path.join(settings.BASE_DIR, "chatapp/data"))
+  return data_files
+
+def get_conversation():
+  return
 
 def get_reply():
   """
@@ -44,8 +63,10 @@ def get_reply():
   string
     The output of the LLM based on the latest input message.
   """
+  print("Getting response...")
+  time.sleep(2)
   
-  return "Robot words..."
+  return "Robot words and such"
 
 def store_message(user_id, convo_id, is_user, message):
   """
